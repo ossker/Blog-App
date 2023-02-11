2b134a271485a5a777c3d9312ad6a8670a83cd17
ossker <oskar.bienko@onet.pl>
2023-02-11 19:35:23 +0100

    Django & React setup.

---
+++ django/blog_api/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
